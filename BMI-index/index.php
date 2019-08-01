8690c59b3fde4fc0c65ff66d21d22c05647344ee
Matthijs Boet <m.boet@chello.nl>
2019-08-01 19:36:40 +0200

    created BMI functionality

--- BMI-index/index.php
+++ BMI-index/index.php
@@ -14,7 +14,7 @@
     </div>
   </header>
   <div class="container mt-3">
-    <form action="calculateBMI.php" method="POST">
+    <form method="POST">
       <div class="form-group row">
         <label class="col-sm-2 col-form-label" for="weight">Weight in kg.</label>
         <div class="col-sm-10">
@@ -34,12 +34,60 @@
         </div>
       </div>
     </form>
-    <div class="col-sm-10 align-right">
-      <h2 class="text-secondary">
-        <?php 
-          calculateBMI($weight, $height)
+    <div class="row">
+      <div class="col-sm-2"></div>
+      <div class="col-sm-10 align-right">
+      
+        <?php
+
+        function calculateBMI($weight, $height){
+          $bmi = ($weight / $height / $height) * 10000;
+          $bmiRounded = round($bmi, 1);
+
+          echo "<h2 style='color: ${calculateBMIColor($bmiRounded)}'>Your BMI is ${bmiRounded}</h2>";
+        }
+
+        function calculateBMIColor($bmiRounded){
+          if($bmiRounded <= 18.4){
+            echo "#375FEF";
+          }
+          if($bmiRounded >= 18.5 && $bmiRounded <= 24.9){
+            echo "#39C117";
+          }
+          if($bmiRounded >= 25 && $bmiRounded <= 29.9){
+            echo "#DFAE23";
+          }
+          if($bmiRounded >= 30 && $bmiRounded <= 39.9){
+            echo "#F68A33";
+          }
+          else {
+            echo "#330906";
+          }
+        }
+
+        function calculateIdealWeight($height){
+
+        }
+
+        if(isset($_POST['calculate'])){
+          if (!isset($_POST['weight'])) {
+            return 'Please enter your weight';
+            exit();
+          }
+          if (!isset($_POST['height'])) {
+            return 'Please enter your height';
+            exit();
+          }
+          $weight = filter_var(htmlentities(floatval($_POST['weight'])),FILTER_SANITIZE_NUMBER_FLOAT);
+
+          $height = filter_var(htmlentities(floatval($_POST['height'])),FILTER_SANITIZE_NUMBER_FLOAT);
+
+          calculateBMI($weight, $height);
+        }
+
         ?>
-      </h2>
+      
+    </div>
     </div>
   </div>
 </body>
