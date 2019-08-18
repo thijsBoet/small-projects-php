e67550368be88a84c9f0491973b1b4ddb01f364d
Matthijs Boet <m.boet@chello.nl>
2019-08-18 22:27:38 +0200

    added prepared statements

--- laravelqa/database/pdo.php
+++ laravelqa/database/pdo.php
@@ -10,6 +10,7 @@ try {
   $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
   // set the PDO error mode to exception => development
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
   return $pdo;
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
--- laravelqa/public/includes/quiz.inc.php
+++ laravelqa/public/includes/quiz.inc.php
@@ -2,14 +2,21 @@
     <form action="" method="POST">
       <div name="question" class="question">
       <?php
-          $id_count = 1;
-          
-          $sql = "SELECT * FROM quiz WHERE id=$id_count";
-          foreach ($pdo->query($sql) as $row) {
-            print $row['question'] . "\n";
+          $id = $question = $answer = null;
+
+          $sql = "SELECT id, question, answer FROM quiz";
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute(['id'=>$id, 'question'=>$question, 'answer'=>$answer]);
+          $quiz = $stmt->fetchAll();
+
+          foreach($quiz as $q){
+            echo $q->question . "<br>";
           }
 
-          var_dump(($pdo->query($sql)));
+          $_SESSION["question_id"] = 1;
+          $_SESSION["score"] = 0;
+
+          
         ?>
       </div>
       <input type="text" name="answer" class="answer" autofocus>
